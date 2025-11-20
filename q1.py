6ebcb2d1718b4a226ab9f131992c23e3eca98c7b
eesen23 <eesen23@ku.edu.tr>
2025-11-20 23:25:25 +0300

    prelab 5

--- q1.py
+++ q1.py
@@ -2,4 +2,17 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    if len(s) < 2 :
+        return ""
+    
+    longest = ""
+    
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            sub = s[i:j]
+            
+            if len(sub) >= 2 and sub == sub[::-1]:
+                if len(sub) > len(longest):
+                    longest = sub
+                    
+    return longest
--- q2.py
+++ q2.py
@@ -2,4 +2,15 @@ def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    
+    result = ""
+    
+    for i in s:
+        if result != "" and result[-1] == i:
+            result = result[:-1]
+        else:
+            result = result + i
+        
+    return result
+    
+    
